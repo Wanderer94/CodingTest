85a5c708e30885aad394b69dd76672b0f6a12baf
JunwooPark <wnsdn1994@gmail.com>
2024-06-04 22:22:38 +0900

    푸드 파이트 대회

--- Level1/푸드 파이트 대회/sol.py
+++ Level1/푸드 파이트 대회/sol.py
@@ -1,11 +1,19 @@
+# def solution(food):
+#     answer = ''
+#     ans = ''
+#     re_ans = ''
+#     for i in food:
+#         for j in range(i//2):
+#             print(i//2)
+#             ans += str(food.index(i))
+#             re_ans = str(food.index(i)) + re_ans
+#     answer = ans + "0" + re_ans
+#     return answer
+
 def solution(food):
     answer = ''
     ans = ''
-    re_ans = ''
-    for i in food:
-        for j in range(i//2):
-            print(i//2)
-            ans += str(food.index(i))
-            re_ans = str(food.index(i)) + re_ans
-    answer = ans + "0" + re_ans
+    for i in range(len(food)):
+        ans += str(i) * (food[i]//2)
+    answer = ans + "0" + ans[::-1]
     return answer
