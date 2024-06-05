2455cec2e8b7b79baec1ddbb5477d6358fe826ee
JunwooPark <wnsdn1994@gmail.com>
2024-06-05 14:55:20 +0900

    가장 가까운 같은 글자 fix

--- Level1/가장 가까운 같은 글자/sol.py
+++ Level1/가장 가까운 같은 글자/sol.py
@@ -1,13 +1,10 @@
 def solution(s):
-    answer = [0 for _ in range(len(s))]
+    answer = []
+    s_dict = dict()
     for i in range(len(s)):
-        if s.count(s[i]) == 1:
-            answer[i] = -1
-            # print("if", answer)
-        elif s.find(s[i]) == i:
-            answer[i] = -1
+        if s[i] not in s_dict:
+            answer.append(-1)
         else:
-            a = s.find(s[i])
-            # print(s[i+1:],a)
-            answer[i] = i - a
+            answer.append(i-s_dict[s[i]])
+        s_dict[s[i]] = i
     return answer
