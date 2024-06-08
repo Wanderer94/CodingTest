09b4aa6a7b325bde9b1fabd8d38633871d602724
JunwooPark <wnsdn1994@gmail.com>
2024-06-08 22:16:36 +0900

    가장 많이 받은 선물fix

--- Level1/가장 많이 받은 선물/sol.py
+++ Level1/가장 많이 받은 선물/sol.py
@@ -10,12 +10,16 @@ def solution(friends, gifts):
         gift_table[x][y] += 1
     col_sum = gift_table.sum(axis = 1)
     row_sum = gift_table.sum(axis = 0)
+    print(gift_table, col_sum, row_sum )
     for x in range(len(friends)):
         for y in range(len(friends)):
             if x != y:
+                # 선물 주고 받은 기록으로 선물주기
                 if gift_table[x][y] > gift_table[y][x]:
+                    print(x, y)
                     friend_point[x] += 1
-                elif gift_table[x][y] == 0 and gift_table[y][x] == 0:
+                # 선물기록이 없을때 혹은 같은경우
+                if gift_table[x][y] == gift_table[y][x]:
                     if col_sum[x] - row_sum[x] > col_sum[y] - row_sum[y]:
                         print(x,y)
                         friend_point[x] += 1
