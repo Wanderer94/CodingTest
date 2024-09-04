c99562a38c79f75c7448b74ac7d2dc561adf64fe
Junwoo Park <60542972+Wanderer94@users.noreply.github.com>
2024-09-04 23:28:36 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,6 @@
+function solution(storey) {
+  if (storey < 5) return storey;
+  const r = storey % 10;
+  const m = (storey - r) / 10;
+  return Math.min(r + solution(m), 10 - r + solution(m + 1)); // 재귀를 활용하여 최소값을 탐색
+}
